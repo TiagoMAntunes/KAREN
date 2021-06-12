0fd2432a3a9ea71d9eb913c9e4440b3a75108cbe
TiagoMAntunes <tiago.melo.antunes@gmail.com>
2021-06-12 01:09:03 +0100

    Added CommonCrawl and Wikipedia embeddings from GloVe

--- karen/toolkit/embeddings.py
+++ karen/toolkit/embeddings.py
@@ -33,8 +33,6 @@ def download(url, savelocation):
     print()
     return True
 
-
-@RegisterEmbedding("Glove")
 class Glove(BaseEmbedding):
     """
     GloVe embeddings
@@ -61,7 +59,7 @@ class Glove(BaseEmbedding):
             for name in tuple(filter(lambda x: x.endswith(".txt"), os.listdir(NAME))):
                 filename = NAME + name
                 embeddings = {}
-                dimension = int(name.split(".")[3][:-1])
+                dimension = int(name.split(".")[-2][:-1])
                 with open(filename, encoding="utf-8") as f:
                     for line in f:
                         line = line.split()
@@ -79,11 +77,28 @@ class Glove(BaseEmbedding):
                 del embeddings
 
     @classmethod
+    def get(cls, **kwargs):
+        cls.process(kwargs['URL'], kwargs['NAME'])
+
+        files = kwargs['FILES']
+
+        if kwargs['dim'] not in files:
+            raise ValueError(f"Unavailable embedding dim {kwargs['dim']} for Glove Embeddings")
+
+        with open(kwargs['NAME'] + files[kwargs['dim']] + ".vocab") as f:
+            vocab = f.read().split()
+
+        return vocab, np.load(kwargs['NAME'] + files[kwargs['dim']] + ".embeddings.npy")
+
+
+@RegisterEmbedding('TwitterGlove')
+class TwitterGlove(Glove):
+    
+    @classmethod
     def get(cls, dim=200):
         URL = "http://downloads.cs.stanford.edu/nlp/data/wordvecs/glove.twitter.27B.zip"
         NAME = SAVEFOLDER + "glove_twitter/"
-        cls.process(URL, NAME)
-
+        
         files = {
             25: "glove.twitter.27B.25d.txt",
             50: "glove.twitter.27B.50d.txt",
@@ -91,10 +106,36 @@ class Glove(BaseEmbedding):
             200: "glove.twitter.27B.200d.txt",
         }
 
-        if dim not in files:
-            raise ValueError(f"Unavailable embedding dim {dim} for Glove Embeddings")
+        return super(TwitterGlove, cls).get(URL=URL, NAME=NAME, FILES=files, dim=dim)
 
-        with open(NAME + files[dim] + ".vocab") as f:
-            vocab = f.read().split()
 
-        return vocab, np.load(NAME + files[dim] + ".embeddings.npy")
+@RegisterEmbedding('CommonGlove')
+class CommonGlove(Glove):
+
+    @classmethod
+    def get(cls, dim=200):
+        URL = "https://nlp.stanford.edu/data/wordvecs/glove.42B.300d.zip"
+        NAME = SAVEFOLDER + "glove_common/"
+
+        files = {
+            300: "glove.42B.300d.txt"
+        }
+
+        return super(CommonGlove, cls).get(URL=URL, NAME=NAME, FILES=files, dim=dim)
+
+@RegisterEmbedding('WikipediaGlove')
+class WikipediaGlove(Glove):
+
+    @classmethod
+    def get(cls, dim=300):
+        URL = 'https://nlp.stanford.edu/data/wordvecs/glove.6B.zip'
+        NAME = SAVEFOLDER + 'glove_wikipedia/'
+
+        files = {
+            50: "glove.6B.50d.txt",
+            100: "glove.6B.100d.txt",
+            200: "glove.6B.200d.txt",
+            300: "glove.6B.300d.txt"
+        }
+
+        return super(WikipediaGlove, cls).get(URL=URL, NAME=NAME, FILES=files, dim=dim)
